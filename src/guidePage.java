85c3c7f3ed0d9864906157aa7f4694a5132c259d
NguyenVanAnit <annit1910@gmail.com>
2023-06-18 18:16:01 +0700

    1

--- src/guidePage.java
+++ src/guidePage.java
@@ -14,33 +14,19 @@ public class guidePage extends JPanel{
     public guidePage(JFrame frame){
         this.frame = frame;
         JButton backButton = new JButton("Quay về trang chủ");
-        JLabel guideLabel1 = new JLabel();
-        JLabel guideLabel2 = new JLabel();
+        JLabel guideLabel = new JLabel();
+        ImageIcon help = new ImageIcon("help.png");
+        JPanel panel = new JPanel();
 
-        ImageIcon banphim = new ImageIcon("banphim.png");
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
+        guideLabel.setIcon(help);
+        guideLabel.setBounds(0, 0, 1000, 1000);
+        panel.add(guideLabel);
+        this.add(panel);
 
-        panel1.setBounds(0, 0, 625, 900);
-        panel1.setBackground(Color.GREEN);
-
-        panel2.setBounds(625, 0, 625, 900);
-        panel2.setBackground(Color.BLUE);
-        panel2.setLayout(null);
-        
-        guideLabel2.setIcon(banphim);
-        guideLabel2.setText("ALo alo");
-        guideLabel2.setBounds(100,0,500,500);
-
-        panel2.add(guideLabel2);
-
-        this.add(panel1);
-        this.add(panel2);
-
-        guideLabel1.setText("<html>Đây là hướng dẫn dòng 1<br/>Đây là hướng dẫn dòng 2 <br/>Đây là hướng dẫn dòng 3 <br/>Dây là hướng dẫn dòng 4</html>");
-        guideLabel1.setFont(new Font("sans-serif", Font.BOLD, 30));
+        guideLabel.setText("<html>Đây là hướng dẫn dòng 1<br/>Đây là hướng dẫn dòng 2 <br/>Đây là hướng dẫn dòng 3 <br/>Dây là hướng dẫn dòng 4</html>");
+        guideLabel.setFont(new Font("sans-serif", Font.BOLD, 30));
         this.add(backButton);
-        this.add(guideLabel1);
+        //this.add(guideLabel);
         this.setLayout(null);
         backButton.addActionListener(e -> {
             this.setVisible(false);
