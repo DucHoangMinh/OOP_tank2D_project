f87c375edb5e0ab0fbae4417bdb894c2debad04e
Duc Ohlin <hoangminhduc4125@gmail.com>
2023-06-29 21:57:45 +0700

    An chỉnh sửa startMenu

--- src/startMenu.java
+++ src/startMenu.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;	
 import javax.sound.sampled.*;	
 import javax.sound.sampled.AudioInputStream;	
@@ -38,38 +40,79 @@ public class startMenu extends JPanel implements ActionListener{
         
         JButton startButton = new JButton("Vào trò chơi");
         JButton viewGuide = new JButton("Xem hướng dẫn");
+        JButton quitButton = new JButton("Thoát game");
         JLabel startLabel = new JLabel("Xe tăng đối kháng");
         JLabel imageLabel = new JLabel();
         JLabel imageLabel2 = new JLabel();
         imageLabel.setIcon(resizedImageIcon);
         imageLabel2.setIcon(BOW);
 
-        Font font = new Font("Arial", Font.BOLD, 16);
+        Font font = new Font("Arial", Font.BOLD, 20);
         startButton.setFont(font);
         viewGuide.setFont(font);
+        quitButton.setFont(font);
         // Đặt màu nền
         Color backgroundColor = new Color(207,177,139);
         startButton.setBackground(backgroundColor);
         viewGuide.setBackground(backgroundColor);
+        quitButton.setBackground(backgroundColor);
         // Đặt màu chữ
         Color textColor = Color.WHITE;
         startButton.setForeground(textColor);
         viewGuide.setForeground(textColor);
+        quitButton.setForeground(textColor);
         // Đặt viền
         startButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         viewGuide.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        quitButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         startButton.setBounds(700, 350, 400, 50);
         viewGuide.setBounds(700, 450, 400, 50);
+        quitButton.setBounds(700, 550, 400, 50);
         imageLabel.setBounds(-10, 300, 600, 400);
         imageLabel2.setBounds(1050, -20, 300 , 300);
         startLabel.setFont(new Font("Tahoma", Font.BOLD, 55));
         startLabel.setBounds(650, 200,550, 100);
 
+         //Setup phương thức khi trỏ chuột vào thì đổi màu, khi không trỏ chuột thì quay về ban đầu
+        startButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e){
+                startButton.setBackground(new Color(250, 200, 100));
+                startButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 5)); 
+                startButton.setBorderPainted(true); 
+            }
+            public void mouseExited(MouseEvent e){
+                startButton.setBackground(new Color(207, 177, 139));
+                startButton.setBorderPainted(false);
+            }
+        });
+        viewGuide.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e){
+                viewGuide.setBackground(new Color(250, 200, 100));
+                viewGuide.setBorder(BorderFactory.createLineBorder(Color.WHITE, 5)); 
+                viewGuide.setBorderPainted(true); 
+            }
+            public void mouseExited(MouseEvent e){
+                viewGuide.setBackground(new Color(207, 177, 139));
+                viewGuide.setBorderPainted(false);
+            }
+        });
+        quitButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e){
+                quitButton.setBackground(new Color(250, 200, 100));
+                quitButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 5)); 
+                quitButton.setBorderPainted(true); 
+            }
+            public void mouseExited(MouseEvent e){
+                quitButton.setBackground(new Color(207, 177, 139));
+                quitButton.setBorderPainted(false);
+            }
+        });
         this.setLayout(null);
         // this.setSize(1000, 1000);
         this.add(startLabel);
         this.add(startButton);
         this.add(viewGuide);
+        this.add(quitButton);
         this.add(imageLabel);
         this.add(imageLabel2);
 
@@ -104,7 +147,10 @@ public class startMenu extends JPanel implements ActionListener{
             frame.repaint();	
             guidePage.requestFocus();	
         });
-        
+        quitButton.addActionListener(e -> {
+            this.setVisible(false);
+            frame.dispose();
+        });
         startButton.addActionListener(e -> {
             this.setVisible(false);
             Gameplay GamePlay = new Gameplay();
